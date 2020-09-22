bc04b57518fbd241b73e6fc2c8dc206c7ac75db3
Purinut Thedwichienchai <purinatth@gmail.com>
2020-09-22 21:16:56 +0700

    finish mvc

--- mvc/mvc/controllers/main_controller.py
+++ mvc/mvc/controllers/main_controller.py
@@ -4,19 +4,23 @@ class MainController:
     def __init__(self):
         # Create note repository here
         # Your code here
+        self.noteRepo = NoteRepository()
         pass
     
     def get_all_notes(self):
         # Return all notes
         # Your code here
+        return self.noteRepo.get_all_notes()
         pass
 
     def add_note(self, note: str):
         # Add note
         # Your code here
+        self.noteRepo.add_note(note)
         pass
 
     def clear_all(self):
         # Clear all note
         # Your code here
+        self.noteRepo.clear_all_notes()
         pass
